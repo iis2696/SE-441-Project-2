b89f50fb4eaba8d19734240c4037540c093d405f
Ian Salitrynski <iis2696@rit.edu>
2013-01-28 17:32:06 -0500

    Worked on SecurityQueue
    Not sure how to initialize it with the scanner ActorRefs. Need to double
    check behavior. Otherwise should be good to go.

--- src/SecurityQueue.java
+++ src/SecurityQueue.java
@@ -8,6 +8,7 @@ public class SecurityQueue extends UntypedActor{
 	private final ActorRef bodyScan;
 	private final ActorRef bagScan;	
 	private Queue<Passenger> passengers = new LinkedList<Passenger>();
+	private boolean scanReady = false;
 	
 	@Override
 	public void onReceive(Object m) throws Exception {
@@ -18,10 +19,22 @@ public class SecurityQueue extends UntypedActor{
 			bodyScan.tell(m);
 			bagScan.tell(m);
 		} else if (m instanceof MessageSendPassenger) {
+			bagScan.tell(m);
 			passengers.add(((MessageSendPassenger) m).getPassenger());
+			if (!passengers.isEmpty() && scanReady) {
+				Passenger p = passengers.poll();
+				MessageSendPassenger msg = new MessageSendPassenger(p);
+				bodyScan.tell(msg);
+				scanReady = false;
+			}
+		} else if (m instanceof MessageReady) {
 			if (!passengers.isEmpty()) {
 				Passenger p = passengers.poll();
 				MessageSendPassenger msg = new MessageSendPassenger(p);
+				bodyScan.tell(msg);
+				scanReady = false;
+			} else {
+				scanReady = true;
 			}
 		}
 		
