a3d51fa5bf509c51ab654e37727c930906dda1bb
Ian Salitrynski <iis2696@rit.edu>
2013-01-28 17:27:57 -0500

    Working on my classes

--- src/DocumentCheck.java
+++ src/DocumentCheck.java
@@ -1,10 +1,40 @@
 import akka.actor.UntypedActor;
+import akka.actor.ActorRef;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class DocumentCheck extends UntypedActor{
-
+	
+	private int cur = 0;
+	private ArrayList<ActorRef> queues = new ArrayList<ActorRef>();
+	private Random r = new Random();
+	
 	@Override
-	public void onReceive(Object arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void onReceive(Object m) throws Exception {
+		
+		if (m instanceof MessageStartDay) {
+			// TODO: Initialize queues array list
+			
+			for (int i = 0; i < queues.size(); i++) {
+				queues.get(i).tell(m);	// Send start day message
+			}
+		} else if (m instanceof MessageEndDay) {
+			
+			for (int i = 0; i < queues.size(); i++) {
+				queues.get(i).tell(m);	// Send end day message
+			}
+		} else if (m instanceof MessageSendPassenger) {
+			System.out.println("Document Check recieved passenger " + ((MessageSendPassenger) m).getPassenger().getID());
+			if (r.nextDouble() > 0.2) {
+				MessageSendPassenger msg = new MessageSendPassenger(((MessageSendPassenger) m).getPassenger());
+				queues.get(cur).tell(msg);
+				System.out.println("Document Check sending passenger" + ((MessageSendPassenger) m).getPassenger().getID() + " to Security Queue " + cur + ".");
+				cur++;
+			} else {
+				System.out.println("Document Check sending passenger" + ((MessageSendPassenger) m).getPassenger().getID() + " away.");
+			}
+		}
 		
 	}
 
--- src/SecurityQueue.java
+++ src/SecurityQueue.java
@@ -1,10 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 
 public class SecurityQueue extends UntypedActor{
-
+	private final ActorRef bodyScan;
+	private final ActorRef bagScan;	
+	private Queue<Passenger> passengers = new LinkedList<Passenger>();
+	
 	@Override
-	public void onReceive(Object arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void onReceive(Object m) throws Exception {
+		if(m instanceof MessageStartDay) {
+			bodyScan.tell(m);
+			bagScan.tell(m);
+		} else if (m instanceof MessageEndDay) {
+			bodyScan.tell(m);
+			bagScan.tell(m);
+		} else if (m instanceof MessageSendPassenger) {
+			passengers.add(((MessageSendPassenger) m).getPassenger());
+			if (!passengers.isEmpty()) {
+				Passenger p = passengers.poll();
+				MessageSendPassenger msg = new MessageSendPassenger(p);
+			}
+		}
 		
 	}
 
