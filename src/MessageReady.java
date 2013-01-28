58e61ef71c2ce4ed9c73eeb97afa78f6a3bebd25
Ian Salitrynski <iis2696@rit.edu>
2013-01-28 17:27:43 -0500

    Forgot this message class

---
+++ src/MessageReady.java
@@ -0,0 +1,14 @@
+/**
+ * immutable message for BodyScan ready
+ * @author Ian Salitrynski
+ *
+ */
+public class MessageReady {
+	
+	/**
+	 * Makes a new immutable message stating scanner ready
+	 */
+	public MessageReady() {
+		// Does nothing
+	}
+}
