a2e061933986a3abea89d12943530419db2ec35e
Mike Lopez <lopes92290@gmail.com>
2013-02-01 13:33:18 -0500

    Completed Jail actor.

--- src/Jail.java
+++ src/Jail.java
@@ -1,11 +1,62 @@
+import java.util.ArrayList;
+
 import akka.actor.UntypedActor;
 
+/**
+ * Receives passengers from security stations.
+ * Transports passengers to detention center
+ * at end of day.
+ * 
+ * @author Mike Lopez
+ *
+ */
 public class Jail extends UntypedActor{
-
+	/*
+	 * The number of security stations in the system.
+	 */
+	private int numberOfSecurityStations;
+	/*
+	 * The list of passengers who have been jailed. 
+	 */
+	private ArrayList<Passenger> jailedPassengers;
+	
+	/**
+	 * Jail constructor.
+	 * 
+	 * @param numberOfSecurityStations - The number of security
+	 * stations in the system.
+	 */
+	public Jail(int numberOfSecurityStations) {
+		this.numberOfSecurityStations = numberOfSecurityStations;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see akka.actor.UntypedActor#onReceive(java.lang.Object)
+	 */
 	@Override
-	public void onReceive(Object arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onReceive(Object m) throws Exception {
+		if(m instanceof MessageSendPassenger) {
+			MessageSendPassenger mp = (MessageSendPassenger) m;
+			
+			System.out.println("Passenger " + mp.getPassenger()
+					+ " has arrived at the Jail.");
+			
+			jailedPassengers.add(mp.getPassenger());
+		} else if(m instanceof MessageEndDay) {
+			System.out.println("Jail received end of day message.");
+			
+			numberOfSecurityStations--;
+			
+			if(numberOfSecurityStations == 0) {
+				for(Passenger p : jailedPassengers) {
+					System.out.println("Passenger " + p.getID()
+							+ " was sent to detention center.");
+				}
+				System.out.println("The Jail is shutting down.");
+				context().stop();
+			}
+		}
 	}
 
 }
