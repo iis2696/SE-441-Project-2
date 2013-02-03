0308ad3e9ec698ad488babcde21fef3d55df4711
Jacob Siegel <jxs1073@g.rit.edu>
2013-02-03 16:22:55 -0500

    work on Driver; don't know if I'm doing this right. ALSO, using newest AKKA so a bunch of stuff is deprecated or changed that you guys have done and I don't know if it works (stop needs to take actorref, as does tell, for example)

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,71 @@
-import akka.actor.UntypedActor;
+import akka.actor.*;
 
+/**
+ * System driver class; not main class, and should
+ * be initialized by main, but starts day, receives
+ * passengers and sends them through TSA screening.
+ * 
+ * At end of day, stops all actors.
+ * 
+ * @author Jacob Siegel
+ */
 public class Driver extends UntypedActor{
-
-	@Override
-	public void onReceive(Object arg0) throws Exception {
-		// TODO Auto-generated method stub
+	private final ActorRef DocCheck;
+	private final int CYCLES;
+	private final MessageStartDay START;
+	private final MessageEndDay END;
+	
+	public Driver(ActorRef dc, int n) {
+		DocCheck = dc;
+		CYCLES = n;
 		
+		START = new MessageStartDay();
+		END = new MessageEndDay();
+		
+		startDay();
+		cyclePassengers();
+	}
+	
+	/*
+	 * Outbound MSG
+	 * - Start Day
+	 * - Passenger Arrival (SendPassenger)
+	 * - End Day (after N cycles)
+	 * 
+	 * Inbound MSG
+	 * - End Day
+	 */
+	
+	@Override
+	public void onReceive(Object m) throws Exception {
+		if ( m instanceof MessageEndDay ) { // System finished
+			context().system().shutdown(); // I think?
+		} else {
+			// do nothing, not supposed to receive other msg
+		}
 	}
 
+	/**
+	 * Simply tells the DocumentCheck actor that the day has
+	 * begun.
+	 */
+	private void startDay() {
+		DocCheck.tell(START);
+		System.out.println("message about sending start to doc check");
+	}
+	
+	/**
+	 * Loops number of times as passed to Driver constructor,
+	 * creating a new passenger and passenger message to send
+	 * to the DocumentCheck actor.
+	 * 
+	 * When loop is completed, sends end-of-day message.
+	 */
+	private void cyclePassengers() {
+		for(int i = 0 ; i < CYCLES; i++) {
+			final MessageSendPassenger passM = new MessageSendPassenger(new Passenger());
+			DocCheck.tell(passM);
+		}
+		DocCheck.tell(END);
+	}
 }
